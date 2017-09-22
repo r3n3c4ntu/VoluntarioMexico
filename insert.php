54b3357b6911d518dc90cc2a6b7ab407e444906d
r3n3c4ntu <renecantumx@gmail.com>
2017-09-21 19:35:57 -0500

    plox

--- insert.php
+++ insert.php
@@ -98,7 +98,7 @@
 	$nombreU= $_POST['nombreU'];
 
 require("connect_db.php");
-mysqli_query($mysqli,"INSERT INTO Centros('Id','Centro','Necesidad','Dirección','Contacto','Hora','Agrada','Desagrada','Asistir','Interesado','Voluntariado','DescripcionVol','Correo','Nombre') VALUES('','$nombre','$material','$direccion','$tel','$horario','0','0','0','0','$opVol','$voluntario', '$mailU', '$nombreU')");
+mysqli_query($mysqli,"INSERT INTO Centros VALUES('','$nombre','$material','$direccion','$tel','$horario','0','0','0','0','$opVol','$voluntario', '$mailU', '$nombreU')");
 ?>
 <div id='card' class="animated fadeIn">
   <div id='upper-side'>
